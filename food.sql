6eddc7d62d74e38b931580ddfd48ea942ca25678
Marcio Pelegrini <marcio.pelegrini@gmail.com>
2022-09-13 15:43:13 -0400

    Update food.sql

--- food.sql
+++ food.sql
@@ -1,10 +1,10 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:8889
--- Généré le : ven. 26 août 2022 à 02:41
--- Version du serveur : 5.7.34
+-- Généré le : mar. 13 sep. 2022 à 19:42
+-- Version du serveur :  5.7.34
 -- Version de PHP : 7.4.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -32,7 +32,7 @@ CREATE TABLE `categories` (
   `nom` varchar(128) NOT NULL,
   `slug` varchar(128) NOT NULL,
   `actif` tinyint(1) NOT NULL DEFAULT '1',
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   `deleted_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -42,9 +42,38 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `nom`, `slug`, `actif`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 'Pizzas traditionnelles', 'pizzas-traditionnelles', 1, '2022-08-25 00:02:14', '2022-08-25 22:08:51', NULL),
-(2, 'Pizzas sucrées', 'pizzas-sucrées', 1, '2022-08-25 00:02:14', '2022-08-24 21:50:19', NULL),
-(3, 'Desserts', 'desserts', 1, '2022-08-25 22:21:10', '2022-08-25 22:35:59', '2022-08-25 22:35:59');
+(1, 'Pizzas traditionnelles', 'pizzas-traditionnelles', 1, '2022-09-12 13:51:32', '2022-09-12 13:51:32', NULL),
+(2, 'Desserts', 'desserts', 1, '2022-09-12 13:51:44', '2022-09-12 13:51:44', NULL),
+(3, 'Pizzas sucrées', 'pizzas-sucrees', 1, '2022-09-12 13:52:06', '2022-09-12 13:52:06', NULL),
+(4, 'Beuvrages', 'beuvrages', 1, '2022-09-12 13:52:24', '2022-09-12 13:52:40', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `extras`
+--
+
+CREATE TABLE `extras` (
+  `id` int(5) UNSIGNED NOT NULL,
+  `nom` varchar(128) NOT NULL,
+  `slug` varchar(128) NOT NULL,
+  `prix` decimal(10,2) NOT NULL,
+  `description` text NOT NULL,
+  `actif` tinyint(1) NOT NULL DEFAULT '1',
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `extras`
+--
+
+INSERT INTO `extras` (`id`, `nom`, `slug`, `prix`, `description`, `actif`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'Bacon importé', 'bacon-importe', '3.00', 'Bacon extra fumé', 1, '2022-09-12 19:56:16', '2022-09-13 15:07:24', NULL),
+(2, 'Fromage', 'fromage-extra', '3.00', 'Extra de fromage rapé', 1, '2022-09-12 19:56:16', '2022-09-13 15:40:03', '2022-09-13 15:40:03'),
+(3, 'Sauce aux tomates', 'sauce-aux-tomates', '0.50', 'Sauce aux tomates frais', 1, '2022-09-13 15:23:13', '2022-09-13 15:23:13', NULL),
+(5, 'Fromage feta', 'fromage-feta', '3.00', 'fromage feta', 1, '2022-09-13 15:35:10', '2022-09-13 15:39:48', NULL);
 
 -- --------------------------------------------------------
 
@@ -69,7 +98,8 @@ CREATE TABLE `migrations` (
 INSERT INTO `migrations` (`id`, `version`, `class`, `group`, `namespace`, `time`, `batch`) VALUES
 (2, '2022-08-15-013041', 'App\\Database\\Migrations\\CreateUserTable', 'default', 'App', 1660571073, 1),
 (3, '2022-08-17-130221', 'App\\Database\\Migrations\\CreationTableUsager', 'default', 'App', 1660741399, 2),
-(5, '2022-08-24-235452', 'App\\Database\\Migrations\\CreerTableCategories', 'default', 'App', 1661385716, 3);
+(4, '2022-08-24-235452', 'App\\Database\\Migrations\\CreerTableCategories', 'default', 'App', 1663005038, 3),
+(5, '2022-08-27-004219', 'App\\Database\\Migrations\\CreerTableExtras', 'default', 'App', 1663005038, 3);
 
 -- --------------------------------------------------------
 
@@ -102,10 +132,10 @@ INSERT INTO `usagers` (`id`, `nom`, `courriel`, `assurance_maladie`, `telephone`
 (1, 'Marcio Andrei gomes', 'mar@mar.ca', 'pelm 123', '', 1, 0, '', NULL, NULL, NULL, '2022-08-17 09:03:33', '2022-08-17 15:22:36', NULL),
 (2, 'Marisa Molaia', 'mamol@mar.ca', 'molm123', '', 0, 0, '', NULL, NULL, NULL, '2022-08-17 13:03:33', '2022-08-17 09:26:33', NULL),
 (3, 'Steve Jobs', 'steve@jobs.ca', 'jobs 8977 9879', '(987) 987-9879', 0, 1, '$2y$10$kSqiCiveae3MaXeABAkJCuEEPFQd0RCZ6UkmeK.pN3ct0RZrfMauS', NULL, NULL, NULL, '2022-08-22 08:11:42', '2022-08-22 09:22:20', '2022-08-22 09:22:20'),
-(4, 'Gill Bates', 'bates@gill.ca', 'bate 0809 8080', '(808) 808-0809', 1, 1, '$2y$10$UhbY2ZOPZ2mMUuoHtvZN6uze8l48NZS0o6nUyXadIF4vIMqTVs5Ea', NULL, NULL, NULL, '2022-08-22 08:12:33', '2022-08-22 20:37:10', NULL),
+(4, 'Gill Bates', 'bates@gill.ca', 'bate 0809 8080', '(808) 808-0809', 1, 1, '$2y$10$yBf51d5ngCDSIJ34V1NvL.QROnkP.DPLX3WkS5qUHJjgtFQpHutfy', NULL, NULL, NULL, '2022-08-22 08:12:33', '2022-08-24 15:01:41', NULL),
 (5, 'Steve Wozniack', 'woz@woz.ca', 'iwur 8923 4598', '(987) 907-0897', 0, 1, '$2y$10$Wi0BYfx8XgcUEp1LYiVVq.LHBHB9Vw2jhRuAHueFAK5MlA1bKsOR2', NULL, NULL, NULL, '2022-08-22 10:19:53', '2022-08-22 10:19:53', NULL),
 (6, 'Jeff Bezos', 'piroca@bezzos.ca', 'bezo 9889 7979', '(897) 897-8979', 0, 1, '$2y$10$b82pVk/al3TRkYVfG7Ht0./iqquiwbn92g2unTWBFD3M59Bk5i82y', NULL, NULL, NULL, '2022-08-22 10:20:28', '2022-08-22 10:20:28', NULL),
-(7, 'Elon Musk', 'musk@tesla.ca', 'musk 0809 8098', '(098) 098-9080', 0, 1, '$2y$10$076Fv6Dlus5OrsfpDlckxedg/Rf7Zn88Yl00txG3seQNRXtJeuyMe', NULL, '837e30a0e1f668c7e0cf656cd7c39116e4275a233e3c7319794828ac807b9eaa', '2022-08-23 23:39:06', '2022-08-22 10:20:59', '2022-08-23 21:39:06', NULL);
+(7, 'Elon Musk', 'musk@tesla.ca', 'musk 0809 8098', '(098) 098-9080', 0, 1, '$2y$10$8qDSb7QSFb72tWw1Z/HRue3ZAKxipaaG3oUcHqWwhUlf4Uj23ZxIS', NULL, '8052b9e134edcfa0959b5cce5200c4994d95aee196e30e2fe18f8f9f76831b5f', '2022-08-24 15:22:22', '2022-08-22 10:20:59', '2022-08-24 14:50:59', NULL);
 
 --
 -- Index pour les tables déchargées
@@ -115,6 +145,13 @@ INSERT INTO `usagers` (`id`, `nom`, `courriel`, `assurance_maladie`, `telephone`
 -- Index pour la table `categories`
 --
 ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nom` (`nom`);
+
+--
+-- Index pour la table `extras`
+--
+ALTER TABLE `extras`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `nom` (`nom`);
 
@@ -141,7 +178,13 @@ ALTER TABLE `usagers`
 -- AUTO_INCREMENT pour la table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `extras`
+--
+ALTER TABLE `extras`
+  MODIFY `id` int(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT pour la table `migrations`
